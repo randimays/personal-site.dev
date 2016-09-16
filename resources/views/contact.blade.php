6685e276aea8832e4180fb4496930a17840bcbac
Randi Mays <randimays1024@gmail.com>
2016-09-16 11:26:57 -0500

    Enabling contact form feature on site.

--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -6,7 +6,7 @@
 	<div class="container">
 		<div class="row">
 			<div class="col-xs-8 col-sm-8 col-md-8 col-lg-8 col-xl-8 col-xs-offset-2 col-sm-offset-2 col-md-offset-2 col-lg-offset-2 col-xl-offset-2">
-				<form action="" method="POST">
+				<form action="https://formspree.io/randimays1024@gmail.com" method="POST">
 					<div class="form-group">
 						<label class="control-label" for="name">name</label>
 						<input id="name" name="name" type="text" placeholder="First Last" class="form-control">
