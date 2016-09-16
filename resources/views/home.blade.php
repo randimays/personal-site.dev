2cd84350015d2ee0e184d83e6eea3c705cf8293e
Randi Mays <randimays1024@gmail.com>
2016-09-16 11:23:04 -0500

    Fixing link on homepage

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,7 +1,7 @@
 @extends('layouts.master')
 @section('content')
 
-<a class="thePeacockLink" href="http://personal-site.dev/about.php#peacock">story of the peacock&nbsp;&nbsp;&nbsp;></a>
+<a class="thePeacockLink" href="{{ action('HomeController@about') }}#peacock">story of the peacock&nbsp;&nbsp;&nbsp;></a>
 
 	<div id="peacockIllustration" class="center-block">
 		<object>
